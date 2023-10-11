fd96dba6f5ebca186216880bed5b71df358f64ae
XXXXsonjisung <jklasd9909@gmail.com>
2023-10-11 14:10:57 +0900

    14:10

--- js_dh.movieT/movieT.js
+++ js_dh.movieT/movieT.js
@@ -1,22 +1,33 @@
-const thumbs = document.querySelectorAll('.fa-thumbs-up');
+document.addEventListener("DOMContentLoaded", function () {
+  const thumbs = document.querySelectorAll('.fa-thumbs-up');
 
-thumbs.forEach((thumb, index) => {
-  thumb.addEventListener('click', () => {
-    if (thumb.classList.contains('far')) {
-      thumb.classList.remove('far');
-      thumb.classList.add('fas');
-      thumb.style.color = '#742AD2'; // 클릭 시 따봉 색 변경 (예: 파란색)
-    } else {
-      thumb.classList.remove('fas');
-      thumb.classList.add('far');
-      thumb.style.color = ''; // 원래 색으로 돌아감
-    }
+  thumbs.forEach((thumb, index) => {
+      thumb.addEventListener('click', () => {
+          if (thumb.classList.contains('far')) {
+              thumb.classList.remove('far');
+              thumb.classList.add('fas');
+              thumb.style.color = 'blue'; // 따봉 색 변경 (예: 파란색)
 
-    // 왼쪽 따봉을 클릭하면, 왼쪽에 있는 모든 따봉을 다 채움
-    for (let i = 0; i < index; i++) {
-      thumbs[i].classList.remove('far');
-      thumbs[i].classList.add('fas');
-      thumbs[i].style.color = '#742AD2'; // 채워진 따봉 색 변경 (예: 파란색)
-    }
+              // 왼쪽 따봉을 클릭하면, 왼쪽에 있는 따봉만 채움
+              for (let i = 0; i < index; i++) {
+                  thumbs[i].classList.remove('far');
+                  thumbs[i].classList.add('fas');
+                  thumbs[i].style.color = 'blue'; // 왼쪽 따봉 색을 채움 (예: 파란색)
+              }
+          } else {
+              thumb.classList.remove('fas');
+              thumb.classList.add('far');
+              thumb.style.color = ''; // 따봉 색 제거
+
+              // 오른쪽 따봉을 클릭하면, 해당 따봉 위치부터 끝까지 리셋
+              for (let i = index + 1; i < thumbs.length; i++) {
+                  thumbs[i].classList.remove('fas');
+                  thumbs[i].classList.add('far');
+                  thumbs[i].style.color = ''; // 오른쪽 따봉 색을 제거
+              }
+          }
+      });
   });
 });
+
+
